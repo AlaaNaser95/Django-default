9d4b54d099a3d3472745abc0075d53c55e494f99
AlaaNaser95 <alaanaser95.95@gmail.com>
2018-12-10 23:59:46 +0300

    create population with signup

--- traffic/models.py
+++ traffic/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 class Population(models.Model):
-    user=models.ForeignKey(User,blank=True,null=True,on_delete=models.CASCADE)
+    user=models.OneToOneField(User,blank=True,null=True,on_delete=models.CASCADE)
     civil_id = models.CharField(max_length=120)
     phone_no= models.CharField(max_length=120,blank=True,null=True)
-    
+@receiver(post_save, sender=User)
+def create_user_population(sender, instance, created, **kwargs):
+    if created:
+        Population.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_population(sender, instance, **kwargs):
+    instance.population.save()
 
 class Accident(models.Model):
     involved= models.ManyToManyField(Population)
