b0980a5929d93d0c2a4c22f54aa1f89aa665f473
sazidahossain <sazidahossain@gmail.com>
2018-12-10 14:58:45 +0300

    Models added

--- traffic/models.py
+++ traffic/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Accident(models.Model):
+    user= models.ForeignKey(User, default=1,  related_name='addresses', on_delete=models.CASCADE)
+    location = models.TextField()
+    date_time = models.DateTimeField(auto_now_add=True)
+    images = models.CharField(max_length=120)
+    status=models.CharField(max_length=120)
+
+class Population(models.Model):
+    civil_id = models.CharField(max_length=120)
+    name = models.TextField()
+    phone_no= models.TextField()
+    
