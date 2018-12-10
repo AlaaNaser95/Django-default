3722aa2b52961f616e0657fd258bec55f40cb3d3
sazidahossain <sazidahossain@gmail.com>
2018-12-10 15:00:31 +0300

    Models added

--- traffic/admin.py
+++ traffic/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Accident
-
+from .models import Population
 admin.site.register(Accident)
+admin.site.register(Population)
