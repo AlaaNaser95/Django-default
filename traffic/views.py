314567eb0a82ef788a168abfd66b5d757b447307
sazidahossain <sazidahossain@gmail.com>
2018-12-11 09:27:01 +0300

    Sending an email works

--- traffic/views.py
+++ traffic/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def email(request):
+    subject = 'Thank you for registering to our site'
+    message = ' it  means a world to us '
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ['sazidahossain@gmail.com',]
+    send_mail( subject, message, email_from, recipient_list )
