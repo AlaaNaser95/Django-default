6e284c906f652b02dafd0315a09f965f935a0350
AlaaNaser95 <alaanaser95.95@gmail.com>
2018-12-16 10:37:29 +0300

    add messages

--- traffic/models.py
+++ traffic/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django import forms
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 
 
 class Profile(models.Model):
@@ -38,7 +38,7 @@ class Accident(models.Model):
     ('Expired','expired')
     }
     status=models.CharField(max_length=120,choices=STATUS,default='Pending')
-
+    
 class CarImage(models.Model):
     accident_image=models.ImageField()
     accident=models.ForeignKey(Accident, on_delete=models.CASCADE)
