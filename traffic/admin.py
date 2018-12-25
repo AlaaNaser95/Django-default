3dee7dfed1ce45406fda4a839bfa52142ac775fb
sazidahossain <sazidahossain@gmail.com>
2018-12-25 12:24:21 +0300

    Comments done

--- traffic/admin.py
+++ traffic/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Accident,Profile,RegistrationImage,CarImage,Report
+from .models import Accident,Profile,RegistrationImage,CarImage,Report,Involved
 
 admin.site.register(Accident)
 admin.site.register(Profile)
 admin.site.register(RegistrationImage)
 admin.site.register(CarImage)
 admin.site.register(Report)
+admin.site.register(Involved)
