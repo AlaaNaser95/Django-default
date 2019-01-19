e4c7b9b30aca467a10a7c9007b6356231f55652d
sazidahossain <sazidahossain@gmail.com>
2019-01-19 13:35:54 +0300

    Last changes made

--- traffic/views.py
+++ traffic/views.py
@@ -26,10 +26,21 @@ def home(request):
     
     return render(request, 'home.html')
 
-def trial(request):
-    
-    return render(request, 'trial1.html')    
+def involved(request):
+    if request.user.is_anonymous:
+        return redirect('login')
+    error=""
+    if request.method=='POST':
+        if request.POST['involved']:
+            num=request.POST['involved']
+            return redirect('create-accident',num)
+        else:
+            error="Please add number of involved people"
+    context = {
+        "error":error,
+    }
 
+    return render(request, "involved.html",context)
 
 def accidentCreate(request,involved=2):
     if request.user.is_anonymous:
@@ -56,10 +67,7 @@ def accidentCreate(request,involved=2):
     accidentForm=AccidentForm()
     commentForm=CommentForm()
     car_images_form=CarImageForm()
-    # car_images_form=modelform_factory(CarImage,form=forms.ModelForm, fields=('accident_image',))
-    """In case Of Post"""
-    # files = request.FILES.getlist('file_field')
-    # print(files)
+
     if request.method == "POST":
         accidentForm = AccidentForm(request.POST, request.FILES)
         involvedFormset = GroupInvolvedFormSet(request.POST, queryset=Profile.objects.none())
@@ -147,69 +155,6 @@ def user_register(request):
     }
     return render(request, 'register.html', context)
 
-
-def user_profile(request):
-    if request.user.is_anonymous:
-        return redirect('login')
-
-    accidents=Accident.objects.filter(involved_people=request.user.profile)
-    profile=request.user.profile
-    prof_form = modelform_factory(Profile, form=forms.ModelForm,fields=('phone_no',),labels={'phone_no':'Mobile Number'})
-    user_form = modelform_factory(User, form=UserRegister,fields=('username','password','email',))
-    user_form = user_form(instance=profile.user)
-    prof_form = prof_form(instance=profile)
-    if request.method == "POST":
-        prof_form = modelform_factory(Profile, form=forms.ModelForm,fields=('phone_no',),labels={'phone_no':'Mobile Number'})
-        user_form = modelform_factory(User, form=forms.ModelForm,fields=('username','password','email',))
-        prof_form = prof_form(request.POST,instance=profile)
-        user_form = user_form(request.POST,instance=request.user)
-        if prof_form.is_valid() and user_form.is_valid():
-            user=user_form.save(commit=False)
-            user.set_password(user.password)
-            user.save()
-            prof_form.save()
-            # login(request.user)
-            # messages.success(request, "Successfully Updated!")
-            return redirect('login')
-    context={
-        "accidents":accidents,
-        "prof_form":prof_form,
-        "user_form":user_form, 
-    }
-    return render(request, 'profile.html',context)
-
-
-
-def updateProfile(request):
-    if request.user.is_anonymous:
-        return redirect('login')
-    profile=request.user.profile
-    prof_form = modelform_factory(Profile, form=forms.ModelForm,fields=('phone_no',),labels={'phone_no':'Mobile Number'})
-    user_form = modelform_factory(User, form=UserRegister,fields=('username','password','email',))
-    user_form = user_form(instance=profile.user)
-    prof_form = prof_form(instance=profile)
-    if request.method == "POST":
-        prof_form = modelform_factory(Profile, form=forms.ModelForm,fields=('phone_no',),labels={'phone_no':'Mobile Number'})
-        user_form = modelform_factory(User, form=forms.ModelForm,fields=('username','password','email',))
-        prof_form = prof_form(request.POST,instance=profile)
-        user_form = user_form(request.POST,instance=request.user)
-        if prof_form.is_valid() and user_form.is_valid():
-            user=user_form.save(commit=False)
-            user.set_password(user.password)
-            user.save()
-            prof_form.save()
-            # login(request.user)
-            # messages.success(request, "Successfully Updated!")
-            return redirect('login')    
-    
-    context = {
-        "prof_form":prof_form,
-        "user_form":user_form,
-        }
-    return render(request, 'updateProfile.html', context)
-
-
-
 def user_login(request):
     form = UserLogin()
     error=""
@@ -236,6 +181,59 @@ def user_login(request):
     }
     return render(request, 'login.html', context)
 
+def user_profile(request):
+    if request.user.is_anonymous:
+        return redirect('login')
+
+    accidents=Accident.objects.filter(involved_people=request.user.profile)
+    profile=request.user.profile
+    prof_form = modelform_factory(Profile, form=forms.ModelForm,fields=('phone_no',),labels={'phone_no':'Mobile Number'})
+    user_form = modelform_factory(User, form=UserRegister,fields=('password','email',))
+    user_form = user_form(instance=profile.user)
+    prof_form = prof_form(instance=profile)
+    if request.method == "POST":
+        prof_form = modelform_factory(Profile, form=forms.ModelForm,fields=('phone_no',),labels={'phone_no':'Mobile Number'})
+        user_form = modelform_factory(User, form=forms.ModelForm,fields=('password','email',))
+        prof_form = prof_form(request.POST,instance=profile)
+        user_form = user_form(request.POST,instance=request.user)
+        if prof_form.is_valid() and user_form.is_valid():
+            user=user_form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+            prof_form.save()
+            return redirect('login')
+    context={
+        "accidents":accidents,
+        "prof_form":prof_form,
+        "user_form":user_form, 
+    }
+    return render(request, 'profile.html',context)
+
+def updateProfile(request):
+    if request.user.is_anonymous:
+        return redirect('login')
+    profile=request.user.profile
+    prof_form = modelform_factory(Profile, form=forms.ModelForm,fields=('phone_no',),labels={'phone_no':'Mobile Number'})
+    user_form = modelform_factory(User, form=UserRegister,fields=('username','password','email',))
+    user_form = user_form(instance=profile.user)
+    prof_form = prof_form(instance=profile)
+    if request.method == "POST":
+        prof_form = modelform_factory(Profile, form=forms.ModelForm,fields=('phone_no',),labels={'phone_no':'Mobile Number'})
+        user_form = modelform_factory(User, form=forms.ModelForm,fields=('username','password','email',))
+        prof_form = prof_form(request.POST,instance=profile)
+        user_form = user_form(request.POST,instance=request.user)
+        if prof_form.is_valid() and user_form.is_valid():
+            user=user_form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+            prof_form.save()
+            return redirect('login')    
+    
+    context = {
+        "prof_form":prof_form,
+        "user_form":user_form,
+        }
+    return render(request, 'updateProfile.html', context)
 
 def user_logout(request):
     logout(request)
@@ -276,8 +274,7 @@ def accidentDetail(request, accident_id):
     involved_comment = Involved.objects.filter(accident=accident)
     car_images=CarImage.objects.filter(accident=accident)
     regis_images=RegistrationImage.objects.filter(accident=accident)
-    # student=classroom.student_set.all().order_by('name','-exam_grade')
-            # messages.success(request, "Successfully booked!")
+
     context = {
     "involved_comment":involved_comment,
         "accident": accident,
@@ -323,8 +320,7 @@ def accidentDetailStaff(request, accident_id):
                 accident.status='Accepted'
                 accident.save()
                 return redirect('staff-accident-list')
-        # student=classroom.student_set.all().order_by('name','-exam_grade')
-                # messages.success(request, "Successfully booked!")
+    
         context = {
             "accident": accident,
             "form":form,
@@ -337,35 +333,7 @@ def accidentDetailStaff(request, accident_id):
         return render(request, 'permission.html')
 
 
-# def reportStaff(request, accident_id):
-#     if request.user.is_anonymous:
-#         return redirect('login')
-#     if request.user.is_staff:
-#         accident = Accident.objects.get(id=accident_id)
-#         #need to check if the user involved in the accident
-#         car_images=CarImage.objects.filter(accident=accident)
-#         regis_images=RegistrationImage.objects.filter(accident=accident)
-#         # student=classroom.student_set.all().order_by('name','-exam_grade')
-#                 # messages.success(request, "Successfully booked!")
-#         form=ReportForm()
-#         if(request.method=='POST'):
-#             form=ReportForm(request.POST)
-#             if form.is_valid():
-#                 report=form.save(commit=False)
-#                 report.accident=accident
-#                 report.detective=request.user
-#                 report.save()
-#                 messages.success(request, "Accident"+str(accident_id)+" is reported successfully")
-#                 return redirect('staff-accident-list')
-#         context = {
-#             "form":form,
-#             "accident": accident,
-#             "car_images":car_images,
-#             "regis_images":regis_images
-#             }
-#         return render(request, 'reportStaff.html', context)
-#     else:
-#         return render(request, 'permission.html')
+
 
 def compliance(request,accident_id,involved_id):
     accident=Accident.objects.get(id=accident_id)
@@ -419,8 +387,6 @@ def send_pdf(request,accident):
     involved = accident.involved_people.all()
     car_images=CarImage.objects.filter(accident=accident)
     regis_images=RegistrationImage.objects.filter(accident=accident)
-        # student=classroom.student_set.all().order_by('name','-exam_grade')
-                # messages.success(request, "Successfully booked!")
     email=[]     
     for involvedx in involved:
         email.append(involvedx.email)           
@@ -428,7 +394,8 @@ def send_pdf(request,accident):
         'report_email.html',
         { "accident": accident,
             "involved": involved,
-            "user":request.user},
+            "user":request.user,
+            "registrationimages":regis_images},
 )
     msg = EmailMessage(
         'Approved report', #subject
@@ -443,19 +410,5 @@ def send_pdf(request,accident):
 def report(request):
    return render(request, 'report.html')
 
-def involved(request):
-    if request.user.is_anonymous:
-        return redirect('login')
-    error=""
-    if request.method=='POST':
-        if request.POST['involved']:
-            num=request.POST['involved']
-            return redirect('create-accident',num)
-        else:
-            error="Please add number of involved people"
-    context = {
-        "error":error,
-    }
 
-    return render(request, "involved.html",context)
 
